59533af507f721389531692ea3c7812e04eee30b
John Paxton <johnpaxton@gmail.com>
2023-01-27 12:43:28 -0500

    Setup for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState } from 'react';
+import MoviesManager from './movies/MoviesManager';
+import FriendsManager from './friends/FriendsManager';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <main className="container">
+      <div className="row">
+        <header className="col">
+          <h1>Favorite Movies App</h1>
+        </header>
+      </div>
+      <div className="row">
+        <div className="col-2">
+          <ul>
+            <li>
+              <Link to="movies">Movies</Link>
+            </li>
+            <li>
+              <Link to="friends">Friends</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="col">
+          <Routes>
+            <Route
+              path="movies"
+              element={<MoviesManager />}
+            />
+            <Route
+              path="friends"
+              element={<FriendsManager />}
+            />
+          </Routes>
+        </div>
+      </div>
+    </main>
   );
 }
 
